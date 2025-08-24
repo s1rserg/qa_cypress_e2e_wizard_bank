b5694289471bb6ad2be6779ebdee513f4769d330
s1rserg <sirserg05@gmail.com>
2025-08-24 19:23:33 +0300

    refactor: improve test structure and assertions for bank account

--- cypress/e2e/hermioneAccount.cy.js
+++ cypress/e2e/hermioneAccount.cy.js
@@ -1,12 +1,15 @@
-/// <reference types='cypress' />
+/* eslint-disable quotes */
+/// <reference types="cypress" />
 
 describe('Bank app', () => {
   before(() => {
-    cy.visit('/');
+    cy.visit(
+      'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login'
+    );
   });
 
-  // eslint-disable-next-line quotes
   it("should provide the ability to work with Hermione's bank account", () => {
+    // login
     cy.contains('.btn', 'Customer Login').click();
     cy.get('#userSelect').select('Hermoine Granger');
     cy.contains('.btn', 'Login').click();
@@ -20,40 +23,44 @@ describe('Bank app', () => {
     cy.contains('.ng-binding', 'Dollar').should('be.visible');
 
     cy.get('[ng-click="transactions()"]').click();
-    cy.get('[ng-click="reset()"]').click();
+    cy.get('table tbody tr').should('have.length', 0);
     cy.get('[ng-click="back()"]').click();
 
     cy.get('[ng-click="deposit()"]').click();
     cy.get('[placeholder="amount"]').type('500');
     cy.contains('[type="submit"]', 'Deposit').click();
-
     cy.get('[ng-show="message"]').should('contain', 'Deposit Successful');
-    cy.contains('[ng-hide="noAccount"]', 'Balance')
-      .contains('strong', '500')
-      .should('be.visible');
 
     cy.get('[ng-click="withdrawl()"]').click();
     cy.contains('[type="submit"]', 'Withdraw').should('be.visible');
     cy.get('[placeholder="amount"]').type('400');
     cy.contains('[type="submit"]', 'Withdraw').click();
-
     cy.get('[ng-show="message"]').should('contain', 'Transaction successful');
     cy.contains('[ng-hide="noAccount"]', 'Balance')
       .contains('strong', '100')
       .should('be.visible');
-    cy.reload();
 
-    cy.get('[ng-class="btnClass1"]').click();
-    cy.get('td.ng-binding').should('contain', 500);
-    cy.get('td.ng-binding').should('contain', 400);
+    cy.get('[ng-click="transactions()"]').click();
+    cy.get('table tbody tr').should('have.length', 2);
+
+    cy.get('table tbody tr')
+      .contains('Credit')
+      .parent()
+      .should('contain', '500');
+    cy.get('table tbody tr')
+      .contains('Debit')
+      .parent()
+      .should('contain', '400');
+
     cy.get('[ng-click="back()"]').click();
 
     cy.get('#accountSelect').select('1002');
     cy.get('[ng-click="transactions()"]').click();
-    cy.get('.table').should('not.contain', 500);
-    cy.get('.table').should('not.contain', 400);
+    cy.get('table tbody tr').should('have.length', 0);
+    cy.get('[ng-click="back()"]').click();
 
     cy.get('.logout').click();
+    cy.contains('Customer Login').should('be.visible');
     cy.url().should('include', '/customer');
   });
 });
